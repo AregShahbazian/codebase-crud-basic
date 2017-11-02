da1829143a1fa8edfd9d2a0c045e42ba2462cbc3
Areg Shahbazian <areg.shahbazian@ing.nl>
2017-11-02 13:46:31 +0100

    axios instead of fetch

--- src/services/api.js
+++ src/services/api.js
@@ -10,32 +10,11 @@ export const DELETE = 'delete'
 
 const API_ROOT = 'http://localhost:9999/'
 
-function callApi(endpoint, schema, method = GET, id = undefined, data = undefined,) {
-    const fullUrl = API_ROOT + endpoint + (id !== undefined ? `/${id}` : "")
+axios.defaults.baseURL = API_ROOT;
+axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-    console.log(`Calling api at ${fullUrl}`)
-
-    return fetch(fullUrl, {
-        method: method,
-        body: data
-    })
-        .then(response =>
-            response.json().then(json => ({json, response}))
-        ).then(({json, response}) => {
-            if (!response.ok) {
-                return Promise.reject(json)
-            }
-
-            return normalize(json, schema);
-        })
-        .then(
-            response => ({response}),
-            error => ({error: error.message || 'Something bad happened'})
-        )
-}
-
-// TODO: handle error returning (and what happens in saga)
-export const bar = (endpoint, schema, method = GET, id = undefined, data = undefined) => {
+export const callApi = (endpoint, schema, method = GET, id = undefined, data = undefined) => {
     let fullEndpoint = endpoint + (id !== undefined ? `/${id}` : "")
     console.info(`Calling api at ${API_ROOT + fullEndpoint}`)
 
@@ -43,18 +22,19 @@ export const bar = (endpoint, schema, method = GET, id = undefined, data = undef
         method: method,
         url: fullEndpoint,
         data: data
-    }).then((response) => response.data).catch((error) => error)
+    }).then((response) => (
+        {response: normalize(response.data, schema)}
+    )).catch((error) => (
+        {error: error}
+    ))
 }
 
 
 const authorSchema = new schema.Entity('data')
 const authorSchemaArray = {data: new schema.Array(authorSchema)};
 
-export const fetchAuthors = () => bar('author', authorSchemaArray)
+export const fetchAuthors = () => callApi('author', authorSchemaArray)
 export const fetchAuthorById = (id) => callApi('author', authorSchema, GET, id)
 export const createAuthor = (newAuthor) => callApi('author', authorSchema, POST, newAuthor)
 
 
-axios.defaults.baseURL = API_ROOT;
-
-export const foo = () => bar('author', authorSchema, GET, 3)
