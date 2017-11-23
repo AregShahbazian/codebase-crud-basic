fac2924b54ce95f233932fadd0037f84ef3bf075
Areg Shahbazian <areg.shahbazian@ing.nl>
2017-11-23 19:15:01 +0100

    saga unit tests

--- src/sagas/index.spec.js
+++ src/sagas/index.spec.js
@@ -1,30 +1,37 @@
 import * as sagas from "./index";
 import {createOperationTypes, entityActions} from "../actions";
-import {callApi, GET, POST} from "../api";
 
 import {testSaga} from "redux-saga-test-plan";
 
 describe('saga', () => {
 
-    const ENTITY_ACTION = 'ENTITY_ACTION'
-
     const OPERATIONS = createOperationTypes('ENTITY')
     const actions = entityActions(OPERATIONS)
 
-    const fetchEntities = () => {
-        return {response: 1, error: 2}
-    }
+    const apiFn = jest.fn();
+
+    const RESPONSE = "response";
+    const ERROR = "error";
 
     it('makeApiCall should put request action, then call api function with action payload, ' +
         'then put success or failure action', () => {
-        testSaga(sagas.makeApiCall, actions.fetchAll, fetchEntities, actions.fetchAll.do())
+        testSaga(sagas.makeApiCall, actions.fetchAll, apiFn, actions.fetchAll.do())
             .next()
             .put(actions.fetchAll.request())
             .next()
-            .call(fetchEntities, actions.fetchAll.do().payload)
+            .call(apiFn, actions.fetchAll.do().payload)
+            .next({response: RESPONSE})
+            .put(actions.fetchAll.success(RESPONSE))
             .next()
-
+            .isDone()
+            .back(2)
+            .next({error: ERROR})
+            .put(actions.fetchAll.failure(ERROR))
+            .next()
+            .isDone()
     })
+
+    const ENTITY_ACTION = 'ENTITY_ACTION'
 
     it('watchAction should watch given action and start given saga', () => {
         testSaga(sagas.watchAction, ENTITY_ACTION, sagas.makeApiCall)
@@ -33,5 +40,6 @@ describe('saga', () => {
             .next()
             .isDone()
     })
+
 })
 
