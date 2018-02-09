81ac9917c21676d29c48f4413c61cae1b46dddd1
Areg Shahbazian <Havasar1>
2018-02-09 15:57:19 +0100

    relational data stub + config !state not looking right

--- src/config/index.js
+++ src/config/index.js
@@ -1,22 +1,29 @@
 import {normalize, schema} from "normalizr";
 import config from "react-global-configuration";
 
-const author = "author";
-const authorSchema = new schema.Entity(author)
-const authorInitialState = normalize([], new schema.Array(authorSchema))
+const publisher = "publisher";
+const publisherSchema = new schema.Entity(publisher)
+const publisherInitialState = normalize([], new schema.Array(publisherSchema))
 
 const book = "book";
 const bookSchema = new schema.Entity(book)
 const bookInitialState = normalize([], new schema.Array(bookSchema))
 
+const author = "author";
+const authorSchema = new schema.Entity('author', {
+    publisher: publisherSchema,
+    books: [bookSchema]
+})
+const authorInitialState = normalize([], new schema.Array(authorSchema))
+
 config.set({
     entities: [
         {
-            entityName: author,
-            endpoint: "author",
-            routineName: "AUTHOR",
-            schema: authorSchema,
-            initialState: authorInitialState
+            entityName: publisher,
+            endpoint: "publisher",
+            routineName: "PUBLISHER",
+            schema: publisherSchema,
+            initialState: publisherInitialState
         },
         {
             entityName: book,
@@ -24,6 +31,13 @@ config.set({
             routineName: "BOOK",
             schema: bookSchema,
             initialState: bookInitialState
+        },
+        {
+            entityName: author,
+            endpoint: "author",
+            routineName: "AUTHOR",
+            schema: authorSchema,
+            initialState: authorInitialState
         }
     ],
     apiRoot: "http://localhost:9999/"
