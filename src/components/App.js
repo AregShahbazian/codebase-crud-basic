6dd64de9fca7f01bb9a953cc57317f95e1764dee
Areg Shahbazian <Havasar1>
2018-02-25 21:01:23 +0800

    removed client side entity filtering

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import AuthorFilter from "./AuthorFilter";
 import ResultsAuthorTable from "../containers/ResultsAuthorTable";
 import AuthorEditor from "../containers/AuthorEditor";
 import AllActionsTest from "../components/AllActionsTest";
 
 const App = () => (
     <div>
-        <AuthorFilter/>
         <ResultsAuthorTable/>
         <AuthorEditor/>
         <AllActionsTest/>
--- src/containers/ResultsAuthorTable.js
+++ src/containers/ResultsAuthorTable.js
@@ -2,20 +2,17 @@ import {concat, forEach} from "lodash";
 import {connect} from "react-redux";
 import AuthorTable from "../components/AuthorTable";
 
-const getFilteredAuthors = (authors, filterFormValues) => {
-    let {nameFilter = ""} = filterFormValues
+const getAuthorsFromState = (authors) => {
     let cachedAuthors = []
 
     forEach(authors.result, (id) => (cachedAuthors = concat(cachedAuthors, authors.entities.author[id])))
 
-    return Object.values(cachedAuthors).filter(a =>
-        a.name.match(new RegExp(nameFilter, 'i'))
-    )
+    return cachedAuthors
 }
 
 
 const mapStateToProps = (state) => ({
-    authors: getFilteredAuthors(state.author, state.form.filterForm.values === undefined ? {} : state.form.filterForm.values)
+    authors: getAuthorsFromState(state.author)
 })
 
 
