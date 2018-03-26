dda543e63e711631210c57e6d2c68652aa291307
Areg Shahbazian <Havasar1>
2018-03-26 16:21:56 +0800

    extracting generic code: state handling only in root component (App)

--- src/components/AuthorApp.js
+++ src/components/AuthorApp.js
@@ -1,14 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import AuthorTableContainer from "../containers/AuthorTableContainer";
 import AuthorEditorContainer from "../containers/AuthorEditorContainer";
 import AllActionsTest from "../components/AllActionsTest";
+import AuthorTable from "./AuthorTable";
 
-const App = () => (
+const AuthorApp = ({authors}) => (
     <div>
-        <AuthorTableContainer/>
+        <AuthorTableContainer
+            authors={authors}
+            entityTableGenerator={(entityRowProps) => <AuthorTable {...entityRowProps}/>}
+        />
         <AuthorEditorContainer/>
         <AllActionsTest/>
     </div>
 )
 
-export default App
+
+AuthorApp.propTypes = {
+    authors: PropTypes.array.isRequired
+}
+
+export default AuthorApp
--- src/containers/AuthorAppContainer.js
+++ src/containers/AuthorAppContainer.js
@@ -1,7 +1,19 @@
 import React from "react";
 import {connect} from "react-redux";
+import {concat, forEach} from "lodash";
 import routines from "../actions/domain";
 import AuthorApp from "../components/AuthorApp";
+
+// TODO: refactor
+const getEntitiesFromState = (entityState, entityName) => {
+    let cachedEntities = []
+    forEach(entityState.result, (id) => (cachedEntities = concat(cachedEntities, entityState.entities[entityName][id])))
+    return cachedEntities
+}
+
+const mapStateToProps = (state) => ({
+    authors: getEntitiesFromState(state.author, "author")
+})
 
 const mapDispatchToProps = ({
     fetchAuthors: routines.AUTHOR.FETCH_ALL.trigger
@@ -9,7 +21,7 @@ const mapDispatchToProps = ({
 
 class AppContainer extends React.Component {
     render() {
-        return <AuthorApp/>
+        return <AuthorApp authors={this.props.authors}/>
     }
 
     componentDidMount() {
@@ -18,7 +30,7 @@ class AppContainer extends React.Component {
 }
 
 AppContainer = connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(AppContainer)
 
